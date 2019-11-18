34ad6bb9a1956f6023bbd4349805cfd3b5689842
Andrew <jenkins.andrew.thomas@gmail.com>
2019-11-18 12:25:22 +0000

    Code to find the magnetic field at r, theta and phi, then outputting those values

--- MagneticField.py
+++ MagneticField.py
@@ -1,25 +1,28 @@
 import numpy as np
-def sph_cart(r, theta, phi):
-    """
-    From Chris' code
-    :param r:
-    :param theta:
-    :param phi:
-    :return:
-    """
-    x = r * np.sin(theta) * np.cos(phi)
-    y = r * np.sin(theta) * np.sin(phi)
-    z = r * np.cos(theta)
-    return x, y, z
+from Magnetic_field_models import field_models
 
-def find_nearest(array, value):
-    """
-    From unutbu on stack overflow
-    :param array:
-    :param value:
-    :return:
-    """
-    array = np.asarray(array)
-    idx = (np.abs(array - value)).argmin()
-    return array[idx]
+fieldGenerator = field_models()
+
+rInRJ = []
+thetaInRadians = []
+phiInRadians = []
+Bradius, Btheta, Bphi = [], [], []
+
+for r in np.arange(1, 10, 1):
+    for theta in np.arange(0.001, 0.5*np.pi, 0.1):
+        for phi in np.arange(0.001, 2*np.pi, 0.1):
+            rInRJ.append(r)
+            thetaInRadians.append(theta)
+            phiInRadians.append(phi)
+            try:
+                Br, Bt, Bp = fieldGenerator.Internal_Field(r, theta, phi, 'JRM09')
+            except:
+                Br, Bt, Bp = np.NaN
+            Bradius.append(Br)
+            Btheta.append(Bt)
+            Bphi.append(Bp)
+
+
+np.savetxt('test.txt', np.c_[rInRJ, thetaInRadians, phiInRadians,
+                             Bradius, Btheta, Bphi], delimiter='\t', header='r\ttheta\tphi\tBr\tBtheta\tBphi')
 
