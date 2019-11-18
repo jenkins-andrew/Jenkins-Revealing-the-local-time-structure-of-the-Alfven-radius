8f14d2f1a0064e90dcd779a6a28104f567d02980
Andrew <jenkins.andrew.thomas@gmail.com>
2019-11-18 15:44:26 +0000

    Plotter for 3D plots

---
+++ PlotFieldLines.py
@@ -0,0 +1,44 @@
+from mpl_toolkits import mplot3d
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def sph_cart(r, theta, phi):
+    """
+    From Chris' code
+    :param r:
+    :param theta:
+    :param phi:
+    :return:
+    """
+    x = r * np.sin(theta) * np.cos(phi)
+    y = r * np.sin(theta) * np.sin(phi)
+    z = r * np.cos(theta)
+    return x, y, z
+
+
+r, theta, phi, Br, Bt, Bp = np.loadtxt('magfieldvalues.txt', delimiter='\t', unpack=True)
+maxR = 10
+minR = 1
+# xtest = np.arange(-maxR, maxR+1, 0.5)
+# ytest = xtest
+# ztest = xtest
+# xtest, ytest, ztest = np.meshgrid(xtest, ytest, ztest)
+x, y, z = sph_cart(r, theta, phi)
+
+mask = (np.sqrt(x**2 + y**2 + z**2) < 2)
+
+B = np.sqrt(Br**2 + Bt**2 + Bp**2)
+
+fig = plt.figure()
+ax = plt.axes(projection="3d")
+
+img = ax.scatter(x[mask], y[mask], z[mask], c=B[mask], cmap=plt.cm.get_cmap('gist_rainbow'))
+clb = fig.colorbar(img)
+clb.ax.set_title(r'B (nT)', fontsize=18)
+ax.set_xlabel('x RJ')
+ax.set_ylabel('y RJ')
+ax.set_zlabel('z RJ')
+plt.show()
+
