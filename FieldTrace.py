df45fddb0da507bc5aafd85adf3ce828ea301b66
Andrew <jenkins.andrew.thomas@gmail.com>
2019-11-18 12:42:08 +0000

    Created a function to find the unit vector

--- FieldTrace.py
+++ FieldTrace.py
@@ -1,4 +1,5 @@
 import numpy as np
+from Magnetic_field_models import field_models
 
 
 def sph_cart(r, theta, phi):
@@ -25,5 +26,27 @@ def find_nearest(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return array[idx]
+
+
+def unitVector(x0, x1, x2):
+    vector = [x0, x1, x2]
+    v_hat = vector / np.sqrt((vector**2).sum())
+    return v_hat[0], v_hat[1], v_hat[2]
+
+
+# r, theta, phi, Br, Btheta, Bphi = np.loadtxt('test.txt', delimiter='\t', unpack=True)
+
+x, y, z = [], [], []
+rInRJ = []
+thetaInRadians = []
+phiInRadians = []
+Bradius, Btheta, Bphi = [], [], []
+
+fieldGenerator = field_models()
+
+Br0, Bt0, Bp0 = fieldGenerator.Internal_Field(10, 0.5*np.pi, 0, 'JRM09')#
+
+
+
 
 
