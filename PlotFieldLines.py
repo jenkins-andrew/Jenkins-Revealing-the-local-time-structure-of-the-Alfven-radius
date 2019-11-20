38918b4aed5907100869afa8182c58b5d6dde86f
Andrew <jenkins.andrew.thomas@gmail.com>
2019-11-20 13:04:29 +0000

    Plot tests and neaten

--- PlotFieldLines.py
+++ PlotFieldLines.py
@@ -23,24 +23,17 @@ x, y, z, B = np.loadtxt('plotmagfieldlines.txt', delimiter='\t', unpack=True)
 
 fig = plt.figure()
 ax = plt.axes(projection="3d")
-plt.plot(x, y, z, '-k')
+#plt.plot(x, y, z, '-k')
 
-# img = ax.scatter(x, y, z, c=B, cmap=plt.cm.get_cmap('gist_rainbow'))
-# clb = fig.colorbar(img)
-# clb.ax.set_title(r'B (nT)', fontsize=18)
+img = ax.scatter(x, y, z, c=B, cmap=plt.cm.get_cmap('gist_rainbow'))
+clb = fig.colorbar(img)
+clb.ax.set_title(r'B (nT)', fontsize=18)
 
 u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
 xs = np.cos(u)*np.sin(v)
 ys = np.sin(u)*np.sin(v)
 zs = np.cos(v)
 ax.plot_surface(xs, ys, zs, color="r")
-
-minimumAxis = min([min(x), min(y), min(z), np.amin(xs), np.amin(ys), np.amin(zs)])
-maximumAxis = max([max(x), max(y), max(z)])
-
-maxz = max(z)
-
-max = 0.5*maximumAxis
 
 max_range = np.array([x.max()-x.min(), y.max()-y.min(), z.max()-z.min()]).max() / 2.0
 
