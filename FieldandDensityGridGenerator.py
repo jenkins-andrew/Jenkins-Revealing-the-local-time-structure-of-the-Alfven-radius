3c2fdccbe2f627ee96338cc9fb7c4c47f5e3347d
Andrew <jenkins.andrew.thomas@gmail.com>
2019-12-04 16:38:09 +0000

    Added plots for Alfven and Radial Velocity

--- FieldandDensityGridGenerator.py
+++ FieldandDensityGridGenerator.py
@@ -5,9 +5,9 @@ import glob
 from scipy.interpolate import griddata
 
 
-def alfvenVelocityFuncForArray(magneticFieldArray, totalMassArray):
+def alfvenVelocityFuncForArray(magneticFieldArray, totalMassDensityArray):
 
-    Va = magneticFieldArray * 1e-9 / np.sqrt(1.25663706212e-6 * totalMassArray)
+    Va = magneticFieldArray * 1e-9 / np.sqrt(1.25663706212e-6 * totalMassDensityArray)
 
     return Va
 
@@ -23,28 +23,29 @@ def radialScaleHeight(r):
     return H
 
 
-def radialVelocityFuncForArray(r, totalMassArray):
+def radialVelocityFuncForArray(r, totalMassDensityArray):
 
-    vr = 500/(2 * totalMassArray * radialScaleHeight(r) * np.pi * r * 71492e3 ** 2)
+    vr = 500/(2 * totalMassDensityArray * radialScaleHeight(r) * np.pi * r * 71492e3 ** 2)
     return vr
 
 
-x, y, z, B, N = [], [], [], [], []
+x, y, z, B, rho = [], [], [], [], []
 
 for field_trace_path in glob.glob('output/postFieldLine/*.txt'):
     print(field_trace_path)
-    x0, y0, z0, B0, N0 = np.loadtxt(field_trace_path, delimiter='\t', unpack=True)
+    x0, y0, z0, B0, rho0 = np.loadtxt(field_trace_path, delimiter='\t', unpack=True)
     x.extend(x0)
     y.extend(y0)
     z.extend(z0)
     B.extend(B0)
-    N.extend(N0)
+    rho.extend(rho0)
 
-np.savetxt('temporaryFile.txt', np.c_[x, y, z, B, N], delimiter='\t')
-x, y, z, B, N = np.loadtxt('temporaryFile.txt', delimiter='\t', unpack=True)
+np.savetxt('temporaryFile.txt', np.c_[x, y, z, B, rho], delimiter='\t')
+x, y, z, B, rho = np.loadtxt('temporaryFile.txt', delimiter='\t', unpack=True)
 
 r = np.sqrt(x**2 + y**2 + z**2)
-
+alfvenVelocity = alfvenVelocityFuncForArray(B, rho)
+radialVelocity = radialVelocityFuncForArray(r, rho)
 
 maxR = 30
 minR = 6
@@ -59,9 +60,14 @@ mask = (xtest < minR) | (np.sqrt(xtest ** 2 + ztest ** 2) > maxR)
 BGrid = griddata((x, z), B, (xtest, ztest))
 BGrid[mask] = np.nan
 
-NGrid = griddata((x, z), N, (xtest, ztest))
+NGrid = griddata((x, z), rho, (xtest, ztest))
 NGrid[mask] = np.nan
 
+AlfvenGrid = griddata((x, z), alfvenVelocity/1000, (xtest, ztest))
+AlfvenGrid[mask] = np.nan
+
+RadialGrid = griddata((x, z), radialVelocity/1000, (xtest, ztest))
+RadialGrid[mask] = np.nan
 
 plt.figure()
 heatmap = plt.contourf(xtest, ztest, BGrid, cmap=plt.cm.get_cmap('gist_rainbow'), alpha=0.4)
@@ -92,6 +98,37 @@ plt.xlim(minR)
 plt.xticks(size=18)
 plt.yticks(size=18)
 plt.tight_layout()
+
+plt.figure()
+plt.rcParams['xtick.labelsize'] = 18
+plt.rcParams['ytick.labelsize'] = 18
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
+plt.tight_layout()
+
+ax = plt.subplot(121)
+heatmap = plt.contourf(xtest, ztest, AlfvenGrid, cmap=plt.cm.get_cmap('gist_rainbow'), alpha=0.4)
+lines = plt.contour(xtest, ztest, AlfvenGrid, 1, colors='k')
+clb = plt.colorbar(heatmap)
+clb.ax.set_title(r'(kms$^{-1}$)', fontsize=18)
+plt.title('Alfven V', fontsize=18, wrap=True)
+plt.xlabel('x $(R_J)$', fontsize=18)
+plt.ylabel('y $(R_J)$', fontsize=18)
+plt.xticks(size=18)
+plt.yticks(size=18)
+plt.xlim(minR)
+
+ax = plt.subplot(122)
+heatmap = plt.contourf(xtest, ztest, RadialGrid, cmap=plt.cm.get_cmap('gist_rainbow'), alpha=0.4)
+lines = plt.contour(xtest, ztest, RadialGrid, 5, colors='k')
+plt.clabel(lines, inline=1, colors='k')
+clb = plt.colorbar(heatmap)
+clb.ax.set_title(r'(kms$^{-1}$)', fontsize=18)
+plt.title('Radial V', fontsize=18, wrap=True)
+plt.xlabel('x $(R_J)$', fontsize=18)
+plt.ylabel('y $(R_J)$', fontsize=18)
+plt.xticks(size=18)
+plt.yticks(size=18)
+plt.xlim(minR)
 
 plt.show()
 
