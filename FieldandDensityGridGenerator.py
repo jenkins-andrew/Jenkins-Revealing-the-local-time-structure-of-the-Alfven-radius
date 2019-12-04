a127044e6f89deeb055afb418b95e0aa3342fffc
Andrew <jenkins.andrew.thomas@gmail.com>
2019-12-04 16:51:05 +0000

    Added plots to make it a multi plot

--- FieldandDensityGridGenerator.py
+++ FieldandDensityGridGenerator.py
@@ -29,7 +29,7 @@ def radialVelocityFuncForArray(r, totalMassDensityArray):
     return vr
 
 
-x, y, z, B, rho = [], [], [], [], []
+x, y, z, B, rho, alfvenPointCheck = [], [], [], [], [], []
 
 for field_trace_path in glob.glob('output/postFieldLine/*.txt'):
     print(field_trace_path)
@@ -46,6 +46,15 @@ x, y, z, B, rho = np.loadtxt('temporaryFile.txt', delimiter='\t', unpack=True)
 r = np.sqrt(x**2 + y**2 + z**2)
 alfvenVelocity = alfvenVelocityFuncForArray(B, rho)
 radialVelocity = radialVelocityFuncForArray(r, rho)
+
+for i in range(len(alfvenVelocity)):
+    if alfvenVelocity[i] > radialVelocity[i]:
+        alfvenPointCheck.append(0)
+    else:
+        alfvenPointCheck.append(1)
+
+np.savetxt('temporaryFile.txt', np.c_[x, r, z, B, rho, alfvenVelocity, radialVelocity, alfvenPointCheck], delimiter='\t')
+x, y, z, B, rho, alfvenVelocity, radialVelocity, alfvenPointCheck = np.loadtxt('temporaryFile.txt', delimiter='\t', unpack=True)
 
 maxR = 30
 minR = 6
@@ -68,6 +77,9 @@ AlfvenGrid[mask] = np.nan
 
 RadialGrid = griddata((x, z), radialVelocity/1000, (xtest, ztest))
 RadialGrid[mask] = np.nan
+
+AlfvenPointGrid = griddata((x, z), alfvenPointCheck, (xtest, ztest))
+AlfvenPointGrid[mask] = np.nan
 
 plt.figure()
 heatmap = plt.contourf(xtest, ztest, BGrid, cmap=plt.cm.get_cmap('gist_rainbow'), alpha=0.4)
@@ -105,7 +117,7 @@ plt.rcParams['ytick.labelsize'] = 18
 plt.subplots_adjust(wspace=0.5, hspace=0.5)
 plt.tight_layout()
 
-ax = plt.subplot(121)
+ax = plt.subplot(221)
 heatmap = plt.contourf(xtest, ztest, AlfvenGrid, cmap=plt.cm.get_cmap('gist_rainbow'), alpha=0.4)
 lines = plt.contour(xtest, ztest, AlfvenGrid, 1, colors='k')
 clb = plt.colorbar(heatmap)
@@ -117,7 +129,7 @@ plt.xticks(size=18)
 plt.yticks(size=18)
 plt.xlim(minR)
 
-ax = plt.subplot(122)
+ax = plt.subplot(222)
 heatmap = plt.contourf(xtest, ztest, RadialGrid, cmap=plt.cm.get_cmap('gist_rainbow'), alpha=0.4)
 lines = plt.contour(xtest, ztest, RadialGrid, 5, colors='k')
 plt.clabel(lines, inline=1, colors='k')
@@ -130,5 +142,29 @@ plt.xticks(size=18)
 plt.yticks(size=18)
 plt.xlim(minR)
 
+ax = plt.subplot(223)
+lines = plt.contour(xtest, ztest, AlfvenPointGrid, 1)
+plt.title('Alfven Radius', fontsize=18, wrap=True)
+plt.xlabel('x $(R_J)$', fontsize=18)
+plt.ylabel('y $(R_J)$', fontsize=18)
+plt.xticks(size=18)
+plt.yticks(size=18)
+plt.xlim(minR)
+
+ax = plt.subplot(224)
+alfvenmask = (alfvenVelocity > 0.95*radialVelocity) & (alfvenVelocity < 1.05*radialVelocity)
+calculatedRadius = np.sqrt(x ** 2 + y ** 2)
+phiwrong = np.arctan2(x, y)
+phi = np.mod(phiwrong, 2*np.pi) * 180 / np.pi
+# fit = np.poly1d(np.polyfit(phi[alfvenmask], calculatedRadius[alfvenmask], 3))
+plt.scatter(phi[alfvenmask], calculatedRadius[alfvenmask], s=0.1, color='k')
+# fitrange = np.arange(0, 360, 1)
+# plt.plot(fit(fitrange))
+plt.title('Alfven Radius', fontsize=18, wrap=True)
+plt.xlabel('Angle (Degrees)', fontsize=18)
+plt.ylabel('Radius (R$_J)$', fontsize=18)
+plt.xticks(size=18)
+plt.yticks(size=18)
+plt.xlim(minR)
 plt.show()
 
