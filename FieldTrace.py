7b53956550928d26da039bde4f44efe39556fc74
Andrew Jenkins <jenkins.andrew.thomas@gmail.com>
2019-12-10 16:09:51 +0000

    Stopped it being a class as that was too complex

--- FieldTrace.py
+++ FieldTrace.py
@@ -55,62 +55,59 @@ def sph_cart(r, theta, phi):
     return x, y, z
 
 
-class FieldTrace(object):
-
-    @staticmethod
-    def produceTraceArrays(currentOn=False, modelType='VIP4'):
-        """
-        To do the field trace using a model over a range of phi and radii. Each phi and radius field trace is saved as a
-        separate text file with the radius and phi value saved in the name. Made as a function such that this could be
-        used as a class in the future.
-        :param modelType: Default VIP4
-        :param currentOn: Default False
-        """
-        printTester = 0
-        fieldGenerator = field_models()
-        signArray = [-1, 1]  # To swap the direction of travel along the field line as well as fix array ordering
-        rmin = 6
-        rmax = 50
-        pmin = 0
-        pmax = 0
-        xInRJ, yInRJ, zInRJ, Bmag = [], [], [], []
-        for phi0 in np.arange(pmin, pmax + 0.001, 0.25 * np.pi):
-            for r0 in np.arange(rmin, rmax + 0.001, 2):
-                # Start a new field line trace
-                for sign in signArray:
-                    # Start a new direction along the field line
-                    theta = 0.5 * np.pi
-                    r = r0
-                    phi = phi0
-                    tempxInRJ, tempyInRJ, tempzInRJ, tempBmag = [], [], [], []  # So I can have two sets of arrays to
-                    # combine later
-                    x, y, z = sph_cart(r, theta, phi)
-                    print('Radius = %5.2f and Phi = %1.2f started going %1.0f' % (r, phi * 180 / np.pi, sign))
-                    while r >= 1:
-                        Br, Bt, Bp, Bx, By, Bz = fieldGenerator.Internal_Field(r, theta, phi, currentOn, modelType)
-                        if printTester % 1 == 0:
-                            tempxInRJ.append(x)
-                            tempyInRJ.append(y)
-                            tempzInRJ.append(z)
-                            tempBmag.append(magnitudeVector(Br, Bt, Bp))
-                        # print(magnitudeVector(Br, Bt, Bp))
-                        # print(magnitudeVector(Bx, By, Bz))
-                        xMove, yMove, zMove = unitVector(Bx, By, Bz)
-                        step = np.log10(magnitudeVector(Bx, By, Bz)) * 10
-                        x += sign * xMove / step
-                        y += sign * yMove / step
-                        z += sign * zMove / step
-                        r, theta, phi = cart_sph(x, y, z)
-                        printTester += 1
-                    # Flipping the arrays if they need to be before putting them together and saving the final trace array
-                    tempxInRJ = tempxInRJ[::sign]
-                    tempyInRJ = tempyInRJ[::sign]
-                    tempzInRJ = tempzInRJ[::sign]
-                    tempBmag = tempBmag[::sign]
-                    xInRJ.extend(tempxInRJ)
-                    yInRJ.extend(tempyInRJ)
-                    zInRJ.extend(tempzInRJ)
-                    Bmag.extend(tempBmag)
-            np.savetxt('newoutput/radius%0.0fto%0.0fphi%0.0fCurrentOn=%s.txt' % (rmin, rmax, phi0, currentOn),
-                       np.c_[xInRJ, yInRJ, zInRJ, Bmag], delimiter='\t')
-        pass
+def produceTraceArrays(rmin, rmax, pmin=0, pmax=0, currentOn=False, modelType='VIP4'):
+    """
+    To do the field trace using a model over a range of phi and radii. Each phi and radius field trace is saved as a
+    separate text file with the radius and phi value saved in the name. Made as a function such that this could be
+    used as a class in the future.
+    :param rmin:
+    :param rmax:
+    :param pmin:
+    :param pmax:
+    :param modelType: Default VIP4
+    :param currentOn: Default False
+    """
+    printTester = 0
+    fieldGenerator = field_models()
+    signArray = [-1, 1]  # To swap the direction of travel along the field line as well as fix array ordering
+    xInRJ, yInRJ, zInRJ, Bmag = [], [], [], []
+    for phi0 in np.arange(pmin, pmax + 0.001, 0.25 * np.pi):
+        for r0 in np.arange(rmin, rmax + 0.001, 2):
+            # Start a new field line trace
+            for sign in signArray:
+                # Start a new direction along the field line
+                theta = 0.5 * np.pi
+                r = r0
+                phi = phi0
+                tempxInRJ, tempyInRJ, tempzInRJ, tempBmag = [], [], [], []  # So I can have two sets of arrays to
+                # combine later
+                x, y, z = sph_cart(r, theta, phi)
+                print('Radius = %5.2f and Phi = %1.2f started going %1.0f' % (r, phi * 180 / np.pi, sign))
+                while r >= 1:
+                    Br, Bt, Bp, Bx, By, Bz = fieldGenerator.Internal_Field(r, theta, phi, currentOn, modelType)
+                    if printTester % 1 == 0:
+                        tempxInRJ.append(x)
+                        tempyInRJ.append(y)
+                        tempzInRJ.append(z)
+                        tempBmag.append(magnitudeVector(Br, Bt, Bp))
+                    # print(magnitudeVector(Br, Bt, Bp))
+                    # print(magnitudeVector(Bx, By, Bz))
+                    xMove, yMove, zMove = unitVector(Bx, By, Bz)
+                    step = np.log10(magnitudeVector(Bx, By, Bz)) * 10
+                    x += sign * xMove / step
+                    y += sign * yMove / step
+                    z += sign * zMove / step
+                    r, theta, phi = cart_sph(x, y, z)
+                    printTester += 1
+                # Flipping the arrays if they need to be before putting them together and saving the final trace array
+                tempxInRJ = tempxInRJ[::sign]
+                tempyInRJ = tempyInRJ[::sign]
+                tempzInRJ = tempzInRJ[::sign]
+                tempBmag = tempBmag[::sign]
+                xInRJ.extend(tempxInRJ)
+                yInRJ.extend(tempyInRJ)
+                zInRJ.extend(tempzInRJ)
+                Bmag.extend(tempBmag)
+        np.savetxt('newoutput/radius%0.0fto%0.0fphi%0.0fCurrentOn=%s.txt' % (rmin, rmax, phi0, currentOn),
+                   np.c_[xInRJ, yInRJ, zInRJ, Bmag], delimiter='\t')
+    pass
