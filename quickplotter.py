61ed4fe51f06797ba0f19a32c2c78edfd9a414bd
Andrew Jenkins <jenkins.andrew.thomas@gmail.com>
2020-02-04 16:14:30 +0000

    Added fractional time plot

--- quickplotter.py
+++ quickplotter.py
@@ -7,7 +7,7 @@ plt.figure()
 files = [f for f in os.listdir('travelTimes/')]
 for i in range(len(files)):
     path = "travelTimes/" + files[i]
-    r, t = np.loadtxt(path, delimiter='\t', unpack=True)
+    r, t, ft = np.loadtxt(path, delimiter='\t', unpack=True)
     phi = float(path[48:52])
     phi = round(phi * 180/np.pi)
 
@@ -18,5 +18,23 @@ plt.xticks(size=18)
 plt.yticks(size=18)
 plt.xlabel('Equatorial starting Radius $(R_J)$', fontsize=18)
 plt.ylabel('Alfven Travel Time $(s)$', fontsize=18)
+plt.tight_layout()
+
+plt.figure()
+
+files = [f for f in os.listdir('travelTimes/')]
+for i in range(len(files)):
+    path = "travelTimes/" + files[i]
+    r, t, ft = np.loadtxt(path, delimiter='\t', unpack=True)
+    phi = float(path[48:52])
+    phi = round(phi * 180/np.pi)
+
+    plt.plot(r, ft, label='Phi = %d' % phi)
+
+plt.legend(fontsize=18)
+plt.xticks(size=18)
+plt.yticks(size=18)
+plt.xlabel('Equatorial starting Radius $(R_J)$', fontsize=18)
+plt.ylabel('Fractional time spent in plasma sheet ', fontsize=18)
 plt.tight_layout()
 plt.show()
